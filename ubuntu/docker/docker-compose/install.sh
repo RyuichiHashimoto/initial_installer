d9eb460b9ba31651e7d5cfce262d0179c19a94d2
RyuichiHashimoto <ryuichi.hashimoto.git@gmail.com>
2021-12-25 00:09:35 +0900

    add specific installer

---
+++ ubuntu/docker/docker-compose/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+version=1.29.1
+
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
---
+++ ubuntu/wireshark/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+sudo add-apt-repository ppa:wireshark-dev/stable
+
+sudo apt update
+
+sudo apt install wireshark
